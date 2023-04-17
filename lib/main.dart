522acc7bd1f1f642b328ad987c547dfe5c634f8c
ADMIN <ayush18november@gmail.com>
2023-04-17 12:19:05 -0600

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'Pages/home_page.dart';
 
 void main() {
+  print('Ayush');
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
